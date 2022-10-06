bfc4fe0bdeb55a96f11454d027b0f337d69f90c0
Ilya Novikov <it@noviligx.com>
2022-10-06 14:49:00 +0300

    add greeting

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
+import prompt
+
 
 def main():
     print("Welcome to the Brain Games!")
+    name_user = prompt.string("May I have your name? ")
+    welcome_user(name_user)
 
 
 if __name__ == '__main__':
